e371e197f0b20fb66aac808cebc7100561220ec8
Dima <akmeev2011@yandex.ru>
2023-01-12 22:27:27 +0300

    Initial commit

---
+++ src/main/java/ru/hogwardts/school/HogwardtsApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwardts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwardtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwardtsApplication.class, args);
+	}
+
+}
