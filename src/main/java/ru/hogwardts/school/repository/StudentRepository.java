f19b43a491f960d5bea6146ab22e032cd957d73b
Dima <akmeev2011@yandex.ru>
2023-02-02 22:52:14 +0300

    ДЗ: sql, пагинация (добавил StudentRepository.java)

--- src/main/java/ru/hogwardts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwardts/school/repository/StudentRepository.java
@@ -1,15 +1,31 @@
 package ru.hogwardts.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.hogwardts.school.model.Faculty;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.hogwardts.school.model.Student;
+import ru.hogwardts.school.model.StudentsByDescOrder;
+//import ru.hogwardts.school.model.StudentsByDescOrder;
 
 import java.util.Collection;
+import java.util.List;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     Collection<Student> findStudentByAgeBetween(int min, int max);
 
     Collection<Student> findStudentsByFaculty_Id(long id);
+
+    @Query(value = "SELECT count (id) from student", nativeQuery = true)
+    Integer getCountOfAllStudents();
+
+    @Query(value = "SELECT avg (age) from student", nativeQuery = true)
+    Double getAverageAgeOfStudents();
+
+    @Query(value = "select * from student order by id desc limit 5", nativeQuery = true)
+    List<StudentsByDescOrder> getFiveStudentsByDescOrder();
+
+
 
 }
